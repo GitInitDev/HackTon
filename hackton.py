9fa96929344dd2b9c6ffaa1548d91ecac9058dda
root <root@localhost.localdomain>
2019-12-21 19:38:25 +0530

    Added And Debugged NMAP Module

--- hackton.py
+++ hackton.py
@@ -3,8 +3,8 @@ import sys
 print(" 1 -> NMAP\n 2 -> Metasploit\n 3 -> Any other fuck")
 choice = int(input("\nEnter The Choice : "))
 # NMAP
-if choice == 1 :
-    ip = str(input("Enter The IP Address : "))
+def nmap () :
+    ip = raw_input("Enter The IP Address : ")
     print("""Choose The Scan Types . . .\n
     1 : Intense Scan\n
     2 : Intense Scan Plus UDP\n
@@ -19,22 +19,24 @@ if choice == 1 :
     """)
     scanType = int(input("Enter Your Choice : "))
     if scanType == 1 :
-        os.system("nmap -T4 -A -v %s",%(ip))
+        os.system("nmap -T4 -A -v "+ip)
     elif scanType == 2 :
-        os.system("nmap -sS -sU -T4 -A -v %s",%(ip))
+        os.system("nmap -sS -sU -T4 -A -v "+ip)
     elif scanType == 3 :
-        os.system("nmap -p 1-65535 -T4 -A -v %s",%(ip))
+        os.system("nmap -p 1-65535 -T4 -A -v "+ip)
     elif scanType == 4 :
-        os.system("nmap -T4 -A -v -Pn %s",%(ip))
+        os.system("nmap -T4 -A -v -Pn "+ip)
     elif scanType == 5 :
-        os.system("nmap -sn %s",%(ip))
+        os.system("nmap -sn "+ip)
     elif scanType == 6 :
-        os.system("nmap -T4 -F %s",%(ip))
+        os.system("nmap -T4 -F "+ip)
     elif scanType == 7 :
-        os.system("nmap -sV -T4 -O -F --version-light %s",%(ip))
+        os.system("nmap -sV -T4 -O -F --version-light "+ip)
     elif scanType == 8 :
-        os.system("nmap -sn --traceroute %s",%(ip))
+        os.system("nmap -sn --traceroute "+ip)
     elif scanType == 9 :
-        os.system("nmap %s",%(ip))
+        os.system("nmap "+ip)
     elif scanType == 10 :
-        os.system("nmap -sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script ""default or (discovery and safe)"" %s",%(ip))
+        os.system('nmap -sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script ""default or (discovery and safe)"" '+ip)
+if choice == 1 :
+    nmap()
