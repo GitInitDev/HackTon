85d0918c72a83d0ac452fc264d7e4291d87e60ad
root <root@localhost.localdomain>
2019-12-22 17:52:42 +0530

    Added weevely() *Not Fully Functional

--- hackton.py
+++ hackton.py
@@ -1,13 +1,16 @@
 import os
 import sys
-print(" 1 -> CUPP\n 2 -> NMAP\n 3 -> ...")
+import subprocess
+print(" 1 -> CUPP\n 2 -> NMAP\n 3 -> Weevely\n")
 choice = int(input("\nEnter The Choice : "))
 # CUPP
 def cupp () :
     print("Entering CUPP .||.")
     os.system("cd cupp && python3 cupp.py -i")
+    return "ThankYou"
 # NMAP
 def nmap () :
+    print("Entering NMAP .||.")
     ip = raw_input("Enter The IP Address : ")
     print("""Choose The Scan Types . . .\n
     1 : Intense Scan\n
@@ -44,8 +47,34 @@ def nmap () :
         os.system('nmap -sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script ""default or (discovery and safe)"" '+ip)
     else :
         os.system("nmap -T4 -A -v "+ip)
+    return "ThankYou"
+# Weevely
+def weevely () :
+    print ("Entering Weevely .||.")
+    print ("""Enter The Process . . .\n
+1 -> Generate New Agent\n
+2 -> Command The Target\n
+3 -> Recover An Existing Session\n
+              """)
+    attackType = int(input("Enter The Choice : "))
+    if attackType == 1 :
+        password = raw_input("Enter Password : ")
+        path = raw_input("Enter Path : ")
+        os.system("weevely generate who /root/Desktop/backdoor.php")
+    elif attackType == 2 :
+        url = raw_input("Enter The URL : ")
+        password = raw_input("Enter The Password : ")
+        os.system("weevely "+url+ password)
+    elif attackType == 3 :
+        path = raw_input("Enter The Path : ")
+        os.system("weevely session"+ path)
+    else :
+        os.system("weevely generate who /root/Desktop/sample.php")
+    return "ThankYou"
 # Choice Decider
 if choice == 1 :
     cupp()
-if choice == 2 :
+elif choice == 2 :
     nmap()
+elif choice == 3 :
+    weevely()
